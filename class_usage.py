3eb688d549dde22b50c56fa9f4c5033f26a28f8f
Thomas Bollaert <thomas.bollaert@gmail.com>
2026-09-11 17:31:49 +0200

    Update and further util of the class example files

--- class_usage.py
+++ class_usage.py
@@ -8,8 +8,12 @@ student1 = Science("Michael", 25, 92)
 student2 = Science("Debbie", 24, 93)
 student3 = Science("Thomas", 99, 90, )
 
-print (student1.age)
-print (student2.cob)
-print (student3.cob)
+def main():
+    print (student1.age)
+    print (student2.cob)
+    print (student3.cob)
 
 #create default object fallback if unsure or if not created. e.g.: cob (country of birth)
+    print("you will not see this text if import from different module is succesful")
+if __name__ == "__main__":
+    main()
---
+++ class_usage2.py
@@ -0,0 +1,4 @@
+from class_usage import Science
+
+student4= Science("Anna", 22, 90, "Brazil")
+print(student4.cob)
