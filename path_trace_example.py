646df0c06d0e138aec852a881e503efdc91cc82e
Thomas Bollaert <thomas.bollaert@gmail.com>
2026-09-11 13:50:21 +0200

    test with path trace

--- path_trace_example.py
+++ path_trace_example.py
@@ -1,10 +1,14 @@
+#disable or all lines by pressing control+/
 #retrieve token
 import requests
 from requests.auth import HTTPBasicAuth
 import urllib3
+import time
+import json
 urllib3.disable_warnings()
 
-BASE_URL = "https://sandboxdnac2.cisco.com"
+#BASE_URL = "https://sandboxdnac2.cisco.com"
+BASE_URL= "https://sandboxdnac.cisco.com"
 AUTH_URL = "/dna/system/api/v1/auth/token"
 USERNAME = "devnetuser"
 PASSWORD = "Cisco123!"
@@ -15,52 +19,60 @@ response = requests.post(BASE_URL + AUTH_URL, auth=HTTPBasicAuth(USERNAME,PASSWO
 token = response.json()["Token"]
 headers = {"X-auth-token": token, "content-type": "application/json"}
 
-# Get all hostnames
+# Get all IP-addresses and hostnames of the registered devices in Catalyst Center
 headers = {"X-auth-token": token, "content-type": "application/json"}
 DEVICES_URL = "/dna/intent/api/v1/network-device"
 response = requests.get(BASE_URL + DEVICES_URL, headers=headers, verify=False)
 devices= response.json()
-print(devices)
+print ("=== Registered devices in catalyst center: ===")
+for device in devices["response"]:
+    print ("IP: " + device["managementIpAddress"] + "," + "Hostname: " + device["hostname"])
+
 
 # #determine source ip
 
-# headers = {"X-auth-token": token, "content-type": "application/json"}
-# DEVICES_URL = "/dna/intent/api/v1/network-device"
-# src_ip_address = ""
-# query_string_params = {'hostname': "CSR1Kv-01.boson.com"}
-# response= requests.get(BASE_URL + DEVICES_URL, headers=headers, params=query_string_params, verify= False)
-# src_ip_address = response.json()["response"][0]["managementIpAddress"]
+headers = {"X-auth-token": token, "content-type": "application/json"}
+DEVICES_URL = "/dna/intent/api/v1/network-device"
+src_ip_address = ""
+query_string_params = {'hostname': "sw1"}
+response= requests.get(BASE_URL + DEVICES_URL, headers=headers, params=query_string_params, verify= False)
+src_ip_address = response.json()["response"][0]["managementIpAddress"]
+print ("IP source device: " + src_ip_address)
 
 # #determine destination IP
 
-# headers = {"X-auth-token": token, "content-type": "application/json"}
-# DEVICES_URL = "/dna/intent/api/v1/network-device"
-# dst_ip_address = ""
-# query_string_params = {'hostname': "CSR1Kv-02.boson.com"}
-# response= requests.get(BASE_URL + DEVICES_URL, headers=headers, params=query_string_params, verify= False)
-# dst_ip_address = response.json()["response"][0]["managementIpAddress"]
+headers = {"X-auth-token": token, "content-type": "application/json"}
+DEVICES_URL = "/dna/intent/api/v1/network-device"
+dst_ip_address = ""
+query_string_params = {'hostname': "sw2"}
+response= requests.get(BASE_URL + DEVICES_URL, headers=headers, params=query_string_params, verify= False)
+dst_ip_address = response.json()["response"][0]["managementIpAddress"]
+print ("Destination IP address: " + dst_ip_address)
 
-# #create path trace
-# PATH_TRACE_URL = "/dna/intent/api/v1/flow-analysis"
-# path_trace_payload = {
-#     "sourceIP":src_ip_address,
-#     "destIP": dst_ip_address,
-#     'inclusions': [
-#         "INTERFACE-STATS",
-#         "DEVICE-STATS",
-#         "ACL-TRACE",
-#         "QOS-STATS"
-#     ],
-#     "protocol": "icmp"
-# }
-# response = requests.post(BASE_URL + PATH_TRACE_URL,headers=headers, json=path_trace_payload, verify= False)
-# flow_analysis_id = response.json()["response"]["flowAnalysisID"]
+#create path trace
+PATH_TRACE_URL = "/dna/intent/api/v1/flow-analysis"
+path_trace_payload = {
+     "sourceIP":src_ip_address,
+     "destIP": dst_ip_address,
+     'inclusions': [
+         "INTERFACE-STATS",
+         "DEVICE-STATS",
+         "ACL-TRACE",
+         "QOS-STATS"
+     ],
+     "protocol": "icmp"
+ }
+response = requests.post(BASE_URL + PATH_TRACE_URL,headers=headers, json=path_trace_payload, verify= False)
+flow_analysis_id = response.json()["response"]["flowAnalysisId"]
+print ("Flow analysis ID: " + flow_analysis_id)
 
-# #retrieve path trace
+# #retrieve path trace through json. dumps(string)
 
-# path_trace_id_URL = "/dna/intent/api/v1/flow-analysis/{flow_analysis_id}"
-# response = requests.get(BASE_URL + path_trace_id_URL.format(flow_analysis_id = flow_analysis_id), headers=headers, verify=False)
-# print (response.json()["response"])
-
-# #delete path trace
-# response = requests.delete(BASE_URL + path_trace_id_URL.format(flow_analysis_id=flow_analysis_id),headers=headers, verify=False)
+time.sleep(5)
+path_trace_id_URL = "/dna/intent/api/v1/flow-analysis/{flow_analysis_id}"
+response = requests.get(BASE_URL + path_trace_id_URL.format(flow_analysis_id = flow_analysis_id), headers=headers, verify=False)
+result = (response.json()["response"])
+print(json.dumps(result, indent=2))
+#delete path trace
+response = requests.delete(BASE_URL + path_trace_id_URL.format(flow_analysis_id=flow_analysis_id),headers=headers, verify=False)
+print(response)
