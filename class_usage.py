7f2c43f31ab42a5d87c9ce458f52c1232f023f0b
Thomas Bollaert <thomas.bollaert@gmail.com>
2026-09-11 16:21:13 +0200

    update class_usage.py

---
+++ class_usage.py
@@ -0,0 +1,15 @@
+class Science:
+    def __init__(self, name, age, grade, cob= "Belgium"):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.cob = cob
+student1 = Science("Michael", 25, 92)
+student2 = Science("Debbie", 24, 93)
+student3 = Science("Thomas", 99, 90, )
+
+print (student1.age)
+print (student2.cob)
+print (student3.cob)
+
+#create default object fallback if unsure or if not created. e.g.: cob (country of birth)
